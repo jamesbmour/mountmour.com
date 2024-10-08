1a6f48aee0992704cae57dc07b0255b07904728d
James <jamesbrendamour7@yahoo.com>
2024-10-08 03:17:05 -0600

    Refactor introduction.mdx and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact(), react(), sitemap(), tailwind(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [mdx(), preact(), react(), sitemap(), tailwind(), tailwind({
+    applyBaseStyles: false
+  }), markdoc()],
   base: `/`,
   site: `https://mountmour.com`
 });
