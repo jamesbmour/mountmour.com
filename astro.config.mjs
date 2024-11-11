0042499dcae48b49714e1273f5ddf99a38dc731b
James <jamesbrendamour7@yahoo.com>
2024-11-11 06:51:34 -0500

    refactor: Remove commented-out Starlight integrations from configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,14 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import svelte from '@astrojs/svelte';
-// import starlightDocSearch from '@astrojs/starlight-docsearch';
-// import starlight from '@astrojs/starlight';
+
 import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(),svelte(), preact(), react(), sitemap(), tailwind(), tailwind({
     applyBaseStyles: false
-  }), markdoc(),sitemap()
-],
+  }), markdoc(),sitemap()],
   base: `/`,
   site: `https://mountmour.com`
 });
